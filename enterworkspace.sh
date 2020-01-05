e6439a067d6bb961214c966547144e2e0b5431fa
zhumingxiao <zhumingxiao@megvii.com>
2020-01-05 10:38:50 +0800

    support c++/vue

--- enterworkspace.sh
+++ enterworkspace.sh
@@ -18,12 +18,17 @@ RestartVimYCM()
 StartVimYCM()
 {
 	rm -rf ${WORKSPACE_VIM_YCM_CID}
-	sudo docker run --env GOPATH=${GOPATH} \
+	sudo docker run \
+	    --env GOPATH=${GOPATH} \
+		--env PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/vim/bin:/root/bin" \
 		--volume ${GOPATH}:${GOPATH} \
 		--workdir ${WORKSPACE_VIM_YCM_WORKDIR} \
 		-itd --rm --cidfile ${WORKSPACE_VIM_YCM_CID} ${WORKSPACE_VIM_YCM_IMAGE} bash
 	containerID=`cat ${WORKSPACE_VIM_YCM_CID}`
 	sudo docker cp ~/.vim/plugin ${containerID}:${WORKSPACE_VIM_CONFIG}
+	sudo docker cp ~/bin/ ${containerID}:${WORKSPACE_BIN_CONFIG}
+	sudo docker cp ~/.ssh/id_rsa ${containerID}:/root/.ssh/
+	sudo docker cp ~/.ssh/id_rsa.pub ${containerID}:/root/.ssh/
 	EnterVimYCM
 }
 
--- workspaceconfig.sh
+++ workspaceconfig.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export WORKSPACE_VIM_YCM_CID="${HOME}/bin/.vimycm.cid"
-export WORKSPACE_VIM_YCM_IMAGE="docker.io/tlimit/vim-ycm:latest"
+export WORKSPACE_VIM_YCM_IMAGE="docker.io/tlimit/vim-ycm:2.1"
 export WORKSPACE_VIM_YCM_USER="zhumingxiao"
 export WORKSPACE_VIM_YCM_WORKDIR="/home/${WORKSPACE_VIM_YCM_USER}/go/src"
 export WORKSPACE_VIM_CONFIG="/root/.vim/"
+export WORKSPACE_BIN_CONFIG="/root/"
